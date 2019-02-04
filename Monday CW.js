387317ab58f358669d13f79ca97f2ad8454d2379
rdunavant0 <rdunavant0@gmail.com>
2019-02-04 11:59:23 -0600

    message

---
+++ Monday CW.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    </head>
+    <body>
+    <h1>I am an h1!</h1>
+<p id="first" class="special">Hello</p>
+    <p class="special">Goodbye</p>
+    <p>Hi Again</p>
+<p id="last">Goodbye Again</p>
+</body>
+</html>
+console.log(<p id="first" class="special">Hello</p>)
+console.log(<p class="special">Goodbye</p>)
+bodyElement1.style.background = blue;
+bodyElement3.style.background = yellow;
